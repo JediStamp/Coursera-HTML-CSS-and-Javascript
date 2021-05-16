1be947aa4ad003e6e88ed7a0f370274261c55ca9
Jenne <jkmiklos@hotmail.com>
2021-05-15 20:29:40 -0400

    End Lesson 55

--- docs/week5/js/script.js
+++ docs/week5/js/script.js
@@ -1,20 +1,52 @@
+console.log(document.getElementById("name").value 
+            + " Coursera!");
+console.log(document.querySelector("#name").value 
+            + " Coursera!");
 // Week 5
-// Lesson 53 DOM Manipulation
-console.log(document.getElementById("title"));
-console.log(document instanceof HTMLDocument);
+// Add event handling (lesson 54)
+document.addEventListener("DOMContentLoaded",
+	function (event) {
+		// Lesson 53 DOM Manipulation
+		console.log(document.getElementById("title"));
+		console.log(document instanceof HTMLDocument);
 
-function sayHello () {
-	//let's retrieve the value in the text box
-	var name = document.getElementById("name").value;
-	console.log(name);
-	var msg = "<h2>Hello " + name + "!</h2>";
+		function sayHello (event) {
+			console.log(event);
+			this.textContent = "Said it!";
+			//let's retrieve the value in the text box
+			var name = document.getElementById("name").value;
+			// console.log(name);
+			var msg = "<h2>Hello " + name + "!</h2>";
 
-	if (name === 'student'){
-		var title = document.querySelector("#title").textContent;
-		title += " & Lovin' it LOL";
-		title = document.querySelector("#title").textContent = title;
-	}
+			// document.getElementById("content").textContent = msg;
+			document.getElementById("content").innerHTML = msg;
 
-	document.getElementById("content").textContent = msg;
-	document.getElementById("content").innerHTML = msg;
-}
+			if (name === 'student'){
+				var title = document.querySelector("#title").textContent;
+				title += " & Lovin' it LOL";
+				// we didn't have to select by ID, we could have selected by any css
+				// if there were a bunch of elelments the same, this would affect the
+				// first one
+				document.querySelector("#title").textContent = title;
+			}
+		}
+
+		// Lesson 54
+		// Unobtrusive event binding 
+		// (so we don't have a bunch of events in our JavaScript)
+		document.querySelector("button").addEventListener("click", sayHello);
+		// or could say 
+		// document.querySelector("button").onclick = sayHello;
+		document.querySelector("body").addEventListener("mousemove", 
+			function (event) {
+				if (event.shiftKey === true) {
+				console.log("x: " + event.clientX);
+				console.log("y: " + event.clientY);					
+				}
+
+			});
+
+	});
+
+
+
